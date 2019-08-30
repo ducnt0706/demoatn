aeeeabc66c5866ae50b093ec658e2025f4987ec4
= <=>
2019-08-30 07:26:08 +0700

    update manage page1

--- header.php
+++ header.php
@@ -17,32 +17,7 @@ require_once './SetUp/functions.php';
 if($login==true){
     //if is logi
     ?>
-    <div class="nav">
-    <!--postion make hide bar when small screen-->
 
-    <!--bar when large screen-->
-
-    <span class="bar">Catalogue
-    	<ul class="drop-content">
-    		<li><a href="addcatalogue.php">Add New!</a> </li>
-    		<li><a href="loadcatalogue.php">Edit</a> </li>
-    	</ul>
-    </span>
-    <span class="bar">Product
-    	<ul class="drop-content">
-    		<li><a href="addproduct.php">Add New!</a> </li>
-    		<li><a href="loadproduct.php">Edit</a> </li>
-    	</ul>
-    </span>
-
-    <span id="logintus">
-        <span>Chào <?php echo "AE"; ?></span>
-<span><img src="" alt=""></span>
-<span id="logincontent"><a href="logout.php">Log out</a></span>
-<span class="bar"><a href="index.php">ATN shop</a></span>
-</span>
-
-</div>
 <?php
 }else{
     //if isn't login
